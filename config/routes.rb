6b54fa14addfb8f37c50b0bbf5fc468349613d9a
maryanne-rose <rosemary.njeri@student.moringaschool.com>
2022-09-27 12:44:31 +0300

    rails resourse create code along

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  resources :birds, only: [:index, :show]
+  resources :birds, only: [:index, :show, :create]
+
+  def create
+    bird = Bird.create(name: params[:name], species: params[:species])
+    render json: bird, status: :created
+  end
+  
 end
